8b63375846c9c132dafbb877c20ce5a31e62dd3a
alexponi <alexponi@mail.ru>
2015-09-25 02:12:47 +0500

    edit model thing

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,10 @@
 class Thing < ActiveRecord::Base
+  has_many :thing_tags
+  has_many :tags, through: :thing_tags
+  has_many :thing_locations
+  has many :locations, through: :thing_locations
+  has_many :thing_files
+  has_many :file_names, through: :thing_files
+  has_many :thing_links
+  has_many :link, through: :thing_links
 end
