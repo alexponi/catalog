f2914359f2e8b9c2ece4b6d55bb19b42e19118b8
alexponi <alexponi@mail.ru>
2015-09-25 22:22:09 +0500

    fix model thing

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -2,7 +2,7 @@ class Thing < ActiveRecord::Base
   has_many :thing_tags
   has_many :tags, through: :thing_tags
   has_many :thing_locations
-  has many :locations, through: :thing_locations
+  has_many :locations, through: :thing_locations
   has_many :thing_files
   has_many :file_names, through: :thing_files
   has_many :thing_links
