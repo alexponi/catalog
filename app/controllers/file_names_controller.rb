6acb2009400288236f54a3e4b0cb654faad07725
alexponi <alexponi@mail.ru>
2015-09-26 00:05:58 +0500

    add authentication filter to controllers

--- app/controllers/file_names_controller.rb
+++ app/controllers/file_names_controller.rb
@@ -1,4 +1,5 @@
 class FileNamesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_file_name, only: [:show, :edit, :update, :destroy]
 
   # GET /file_names
--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,4 +1,5 @@
 class ThingsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_thing, only: [:show, :edit, :update, :destroy]
 
   # GET /things
